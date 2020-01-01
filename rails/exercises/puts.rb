2c2c20a55d9a5a08ec3eb2223b907a9cf7a5832c
Brett Jory <bjory7@gmail.com>
2020-01-01 14:28:44 -0800

    initial commit

---
+++ rails/exercises/puts.rb
@@ -0,0 +1,20 @@
+# This is a comment
+
+=begin
+a
+multiline
+		comment
+=end
+
+puts "hello"
+
+END {
+	puts "this is the last block"
+}
+
+puts "Coding"
+puts "Dojo"
+
+BEGIN {
+	puts "this is the first block"
+}
---
+++ rails/exercises/variable.rb
@@ -0,0 +1,19 @@
+x = 5
+y = 10
+z = x + y
+
+puts z
+
+x = 2; y = 3; z = x + y; puts z
+
+first_name = "Chika"
+last_name = "Fujiwara"
+
+puts "Your name is " + first_name + " " + last_name
+
+puts "First name is #{first_name} and last name is #{last_name}"
+
+z = 50
+puts "Value of z is %d" % [z]
+
+puts "I am 6'0\" tall"
